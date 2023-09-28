d31d814521b9df3f6358b0f9ec05c7009d3fce83
jungdawoon <tyranno2020@gmail.com>
2023-09-28 23:24:07 +0900

    update

---
+++ contracts/ex3.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// A Token 만들기
+
+contract TokenA is ERC20 {
+    constructor() ERC20("TokenA","A") {
+
+    }
+}
+
+// etherToTokenSwap
+contract JungSwap {
+
+
+}
