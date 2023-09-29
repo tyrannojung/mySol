e249a2bff0bf6e519884e174f1e1c4eeacf2e756
jungdawoon <tyranno2020@gmail.com>
2023-09-29 18:53:56 +0900

    update

--- contracts/ex3.sol
+++ contracts/ex3.sol
@@ -12,6 +12,36 @@ contract TokenA is ERC20 {
 
 // etherToTokenSwap
 contract JungSwap {
+    // Ether <> A Token Exchange
+    // 그러므로 해당 토큰 address를 가지고 있어야 한다.
+    address tokenAddress;
 
+    function etherToTokenSwap() public payable returns(bool) {
+        uint etherAmount = msg.value;
+        // CSMM: etherAmount + tokenAmount = k
+        uint tokenAmount = etherAmount;
+        // 토큰 주는 행위가 필요하다.
+        TokenA tokenContract = TokenA(tokenAddress);
+        tokenContract.transfer(msg.sender, tokenAmount);
+        return true;
+    }
+
+    function tokenToEtherSwap(uint tokenAmount) public returns(bool) {
+        // uint tokenAmount = amount;
+        // uint etherAmount = tokenAmount;
+        // TokenA tokenContract = TokenA(tokenAddress);
+        // tokenContract.transfer(address(this), amount);
+        // payable(msg.sender).transfer(etherAmount);
+        // 이 컨트랙트 주소가 owner가 아니니, 남의 돈을 빼올 수 없다.
+        
+        // 사용자는 token <> Ether일때 두번 호출한다.
+        // Approve()는 별도의 tx로 진행된 상태
+        TokenA tokenContract = TokenA(tokenAddress);
+        tokenContract.transferFrom(msg.sender, address(this), tokenAmount);
+        uint etherAmount = tokenAmount;
+        payable(msg.sender).transfer(etherAmount);
+
+        return true;
+    }
 
 }
