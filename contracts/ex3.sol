4cc782f3a61323788a38f58f7519eecf75329345
jungdawoon <tyranno2020@gmail.com>
2023-09-29 20:01:10 +0900

    update

--- contracts/ex3.sol
+++ contracts/ex3.sol
@@ -11,10 +11,42 @@ contract TokenA is ERC20 {
 }
 
 // etherToTokenSwap
-contract JungSwap {
+contract JungSwap is ERC20 {
     // Ether <> A Token Exchange
     // 그러므로 해당 토큰 address를 가지고 있어야 한다.
-    address tokenAddress;
+    address public tokenAddress;
+
+    constructor(address _tokenAddress) ERC20("JungLP", "JUNG") {
+        tokenAddress = _tokenAddress;
+    }
+
+    function addLiquidity() public payable returns (bool) {
+        // LP공급자로 부터 Ether와 Token을 받는다. (1, 1) = 1 LP
+        // LP공급자에게 받은 만큼 LP Token을 민팅한다.
+        
+        // a token b token 1 : 1 비율로 입금
+        uint etherAmount = msg.value;
+        uint tokenAmount = etherAmount;
+        TokenA tokenContract = TokenA(tokenAddress);
+        // Approve()는 별도의 tx로 진행된 상태
+        tokenContract.transferFrom(msg.sender, address(this), tokenAmount);
+        _mint(msg.sender, etherAmount);
+        return true;
+    }
+
+    function removeLiquidity(uint lpTokenAmount) public returns(bool) {
+        // require(balances[msg.sender] >= lpTokenAmount);
+        // TokenA tokenContract = TokenA(tokenAddress);
+        // tokenContract.transfer(msg.sender, tokenAmount);
+        // payable(msg.sender).transfer(tokenAmount);
+        _burn(msg.sender, lpTokenAmount);
+        uint etherAmount = lpTokenAmount;
+        uint tokenAmount = lpTokenAmount;
+        payable(msg.sender).transfer(etherAmount);
+        TokenA tokenContract = TokenA(tokenAddress);
+        tokenContract.transfer(msg.sender, tokenAmount);
+        return true;
+    }
 
     function etherToTokenSwap() public payable returns(bool) {
         uint etherAmount = msg.value;
