b532ab4364d14d612b84728d6cbc356c8ae2a4ba
jungdawoon <tyranno2020@gmail.com>
2023-10-02 14:01:07 +0900

    update

--- contracts/ex4.sol
+++ contracts/ex4.sol
@@ -20,5 +20,43 @@ contract Exchange is ERC20 {
 
     }
 
+    // getInputPrice == getOutputamount
+    function getInputPrice(
+        uint inputAmount, 
+        uint inputReserve, 
+        uint outputReserve
+    ) public pure returns (uint outputAmount) {
+
+    }
+
+    // getInputPrice == getOutputamount
+    function getOutputPrice(
+        uint outputAmount,
+        uint inputReserve,
+        uint outputReserve
+    ) public pure returns (uint inputAmount) {
+
+    }
+
+    function etherToTokenOutput(uint tokenAmount, uint maxEtherAmount) public payable {
+        // Token의 갯수가 고정 : Input parameter
+        // MEV Attack
+        uint etherAmount = tokenAmount / 997 * 1000;
+        // Price Discovery Function
+        // (X,Y), (x,y)
+        // (1000, 1500), (100, y) ?
+        // (1000, 1500), (x, 100) ?
+
+
+        // 정확하게 필요한 ETH 수량 <> 유저가 낸 Ether(msg.value)
+        require(maxEtherAmount >= etherAmount);
+        require(msg.value >= etherAmount);
+        uint etherRefundAmount = msg.value - etherAmount;
+        if (etherRefundAmount > 0) {
+            payable(msg.sender).transfer(etherRefundAmount);
+        }
+        ERC20 token = ERC20(tokenAddress);
+        token.transfer(msg.sender, tokenAmount); 
+    }
 
 }
